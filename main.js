1dae5ff47337c5579495246b45fa1ca41c92cc16
LegoLeo7 <99499098+LegoLeo7@users.noreply.github.com>
2022-10-22 19:18:02 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 prediction_1 = ""
 prediction_2 = ""
 Webcam.set({
@@ -10,7 +9,7 @@ png_qualtiy:90
 
 camera = document.getElementById("camera");
 
-webcam.attach( '#camera' );
+Webcam.attach( '#camera' );
 
 function take_snapshot()
 
@@ -22,7 +21,7 @@ function take_snapshot()
 
 console.log('ml5 version:', ml5.version);
 
-classifier = ml5.imageClassifier('https://storage.googleapis.com/tm-model/7ywnzn1ET/model.json',modeLoaded);
+classifier = ml5.imageClassifier('https://storage.googleapis.com/tm-model/T08wH6NAc/model.json/model.json',modeLoaded);
 
 function modelLoaded() {
     console.log('Model Loaded!');  
@@ -31,7 +30,55 @@ function modelLoaded() {
 function speak(){
     var synth = window.speechSynthesis;
     speak_data_1 = "The first prediction is " + prediction_1;
-    speak_data_1 = "The second prediction is " + prediction_2;
-    var utterthis = new SpeechSynthesisisUtterance(speak_date_1 + speak_data_2);
+    speak_data_1 = "and second prediction is " + prediction_2;
+    var utterthis = new SpeechSynthesisisUtterance(speak_data_1 + speak_data_2);
     synth.speak(utterThis);
 }
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+
+
+function gotResult(error, results) {
+    if (error) {
+      console.error(error);
+    } else { 
+        console.log(results) ;
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+            if(results[0].label == "Ok symbol")
+            {
+                document.getElementById("update_emoji").innerHTML = "&#128076";
+            }
+            if(results[0].label == "Good symbol")
+            {
+                document.getElementById("update_emoji").innerHTML = "&#128077";
+            }
+            if(results[0].label == "bad symbol")
+            {
+                document.getElementById("update_emoji").innerHTML = "&#128076";
+            }
+            if(results[1].label == "ok symbol")
+            {
+                document.getElementById("update_emoji2").innerHTML = "&#128076";
+            }
+            if(results[1].label == "Good symbol")
+            {
+                document.getElementById("update_emoji2").innerHTML = "&#128077";
+            }
+            if(results[1].label == "bad symbol")
+            {
+                document.getElementById("update_emoji2").innerHTML = "&#128076";
+            }
+          
+
+    
+        }
+    }
